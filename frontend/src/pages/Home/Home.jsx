4d9b5f12ea327534e822723a51ae34ea93e94015
Luki Centuri <drybutcool@gmail.com>
2021-10-09 22:28:40 +0700

    feat: add real time functionality to home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import css from './Home.module.scss';
 import { useEffect, useState } from "react";
 import httpRequest from "../../lib/http-request";
 import ProductCard from "../../components/ProductCard/ProductCard";
+import { ActionCable } from 'react-actioncable-provider';
 
 const Home = () => {
   const [products, setProducts] = useState([]);
@@ -16,23 +17,32 @@ const Home = () => {
     fetchProducts();
   }, []);
 
+  const handleReceivedProducts = (products) => {
+    setProducts(products);
+  }
+
   if (products.length === 0) return null;
 
   return (
-    <section>
-      <div className="header">
-        <div className="container">
-          <h1>Products</h1>
-        </div>
-      </div>
-      <div className={css.products}>
-        <div className="container">
-          <div className={css.products__grid}>
-            {products.map((product) => <ProductCard key={product.id} product={product} />)}
+    <>
+      <ActionCable channel={{ channel: 'ProductsChannel' }}
+                   onReceived={handleReceivedProducts}
+      ></ActionCable>
+      <section>
+        <div className="header">
+          <div className="container">
+            <h1>Products</h1>
           </div>
         </div>
-      </div>
-    </section>
+        <div className={css.products}>
+          <div className="container">
+            <div className={css.products__grid}>
+              {products.map((product) => <ProductCard key={product.id} product={product} />)}
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 };
 
